b0cd75f0ef81a2361056dfb30ea170f7625c000d
Jozef W <62598812+Jozefwl@users.noreply.github.com>
2023-04-27 21:55:33 +0200

    Install Multer
    Install multer using npm
    Add Upload Image to form
    Add null state for image
    Add multer configuration to server.js

--- client/src/bricks/UploadVideo.js
+++ client/src/bricks/UploadVideo.js
@@ -1,107 +1,139 @@
-import React, { useState } from 'react'
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-export const genres = ['Action', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Programming', 'Romance', 'Thriller', 'Tech', 'Coocking',];
+export const genres = [
+  "Action",
+  "Comedy",
+  "Drama",
+  "Fantasy",
+  "Horror",
+  "Programming",
+  "Romance",
+  "Thriller",
+  "Tech",
+  "Coocking",
+];
 
-
-function newVideo(name, genre, link, language, successF, errorF) {
+function newVideo(name, genre, link, language, image, successF, errorF) {
   fetch("/api/videos", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({name, genre, link, language})
-  }).then(res => res.ok ? successF() : errorF(res))
-  
+    body: JSON.stringify({ name, genre, link, language, image }),
+  }).then((res) => (res.ok ? successF() : errorF(res)));
 }
 
-function UploadVideo({callBackUpload}) {
-  const [hidden, setVisible] = useState(true)
+function UploadVideo({ callBackUpload }) {
+  const [hidden, setVisible] = useState(true);
 
-  const [name, setName] = useState("")
-  const [link, setLink] = useState("")
-  const [language, setLanguage] = useState("")
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+  const [language, setLanguage] = useState("");
 
   const [selectedGenres, setSelectedGenres] = useState("");
 
-  
+  const [image, setImage] = useState(null); // add a state for the image
 
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col md={6}>
-      <Button variant="outline-success" onClick={() => setVisible(!hidden)}>{hidden ? "Upload" : "Cancel"}</Button>
-      <Form hidden={hidden}>
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control 
-          type="text" 
-          placeholder="Name" 
-          value={name}
-          onChange={(e) => setName(e.target.value)} />
-        </Form.Group>
+          <Button variant="outline-success" onClick={() => setVisible(!hidden)}>
+            {hidden ? "Upload" : "Cancel"}
+          </Button>
+          <Form hidden={hidden}>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicGenre">
-        <Form.Label>Genre</Form.Label>
-        <Form.Select
-          value={selectedGenres}
-          onChange={(e) => setSelectedGenres(e.target.value)}
-        >
-          {genres.map((genre, index) => (
-            <option key={index} value={genre}>{genre}</option>
-          ))}
-        </Form.Select>
-      </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicGenre">
+              <Form.Label>Genre</Form.Label>
+              <Form.Select
+                value={selectedGenres}
+                onChange={(e) => setSelectedGenres(e.target.value)}
+              >
+                {genres.map((genre, index) => (
+                  <option key={index} value={genre}>
+                    {genre}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicLink">
-          <Form.Label>Link</Form.Label>
-          <Form.Control 
-          type="text" 
-          placeholder="Link" 
-          value={link}
-          onChange={(e) =>setLink(e.target.value)}/>
-        </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicLink">
+              <Form.Label>Link</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Link"
+                value={link}
+                onChange={(e) => setLink(e.target.value)}
+              />
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicLanguage">
-          <Form.Label>Language</Form.Label>
-          <Form.Control 
-          type="text" 
-          placeholder="Language"
-          value={language} 
-          onChange={(e) =>setLanguage(e.target.value)}/>
-        </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicLanguage">
+              <Form.Label>Language</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Language"
+                value={language}
+                onChange={(e) => setLanguage(e.target.value)}
+              />
+            </Form.Group>
 
-        <Button variant="primary" type="button" 
-          disabled={
-            name === "" ||
-            selectedGenres === "" ||
-            link === "" ||
-            language === "" 
-            }
-            onClick={() => {
-              newVideo(name, selectedGenres, link, language,
-                // succesF
-                () =>  {
-                  setName("")
-                  setSelectedGenres("")
-                  setLink("")
-                  setLanguage("")
-                  callBackUpload()
-              },
-              // errorF
-              (res) => { 
-                console.error(res)
-              })
-            }}
+            <Form.Group className="mb-3" controlId="formBasicLanguage">
+              <Form.Label>Image</Form.Label>
+              <input
+                class="form-control"
+                type="file"
+                id="formFileMultiple"
+                multiple
+              />
+            </Form.Group>
+
+            <Button
+              variant="primary"
+              type="button"
+              disabled={
+                name === "" ||
+                selectedGenres === "" ||
+                link === "" ||
+                language === ""
+              }
+              onClick={() => {
+                newVideo(
+                  name,
+                  selectedGenres,
+                  link,
+                  language,
+                  // succesF
+                  () => {
+                    setName("");
+                    setSelectedGenres("");
+                    setLink("");
+                    setLanguage("");
+                    callBackUpload();
+                  },
+                  // errorF
+                  (res) => {
+                    console.error(res);
+                  }
+                );
+              }}
             >
-          Submit
-        </Button>
-      </Form>
-
-      </Col>
+              Submit
+            </Button>
+          </Form>
+        </Col>
       </Row>
     </Container>
-  )
+  );
 }
 
-export default UploadVideo
+export default UploadVideo;
--- client/src/server/server.js
+++ client/src/server/server.js
@@ -4,13 +4,28 @@ const bodyParser = require('body-parser')
 const app = express()
 const uuidv4 = require("uuid")
 const port = 3001
+const multer = require("multer");
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+//configure multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+  cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+  cb(null, Date.now() + "-" + file.originalname);
+  },
+  });
+
+  const upload = multer({ storage });
+
+//videos database
 const defaultDb = {
   "videosList" : []
 }
+
 function readDB() {
   let path = "./server_data/DB_videos.json"
   if (!fs.existsSync(path)) {
@@ -19,6 +34,7 @@ function readDB() {
   }
   return JSON.parse(fs.readFileSync(path))
 }
+
 
 function saveDB(ObjectDB) {
   let path = "./server_data/DB_videos.json"
